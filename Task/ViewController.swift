4cc69483be6df5451dba86417d50e05148dfb9dd
micasasucasa <micasasucasa@micasasucasas-macbook-pro.local>
2019-01-30 13:40:57 -0700

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by micasasucasa on 1/30/19.
+//  Copyright © 2019 Jacob Rosevear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
